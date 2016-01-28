ff05764515476a6b30022be7f81eb22a3c9a7cac
“lrosa007” <lrosa008@gmail.com>
2016-01-27 23:35:17 -0500

    silly attempt at primes

--- assignments/001/primes.go
+++ assignments/001/primes.go
@@ -1,10 +1,94 @@
+//Lucas Rosa
+//COP 4520
 package main
 
 import (
 	"fmt"
+    "os"
+	"strconv"
 	"time"
 )
 
-func main() {
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
+// Doesn't return a prime literaly, but the least value to be used
+// to filter out non prime numbers from an array or slice
+func nextPrime(a []int, factor int) (newFactor int) {
+    for _, v := range a {
+        if v > factor {
+            newFactor = v
+            break
+        }
+    }
+    return
+}
+
+func main() {
+	t1 := time.Now()
+
+	f, err := os.Create("output.txt")
+    check(err)
+
+    defer f.Close()
+
+    // Create slice containing values from 0 to 1 million
+    a := []int{}
+    for i := 0; i < 1000000; i++ {
+        a = append(a, i)
+    }
+
+    done := false
+    factor := 1
+    for !done {
+        // Get the next factor to be used as a filter for slice `a`
+        factor = nextPrime(a, factor)
+
+        var modified bool
+        // Filtering slice `a`
+        for i, v := range a {
+            if v%factor == 0 && v > factor {
+                a[i] = 0 // This is equivalent to eliminating the value
+                modified = true
+            }
+        }
+
+        if !modified {
+            done = true
+        }
+    }
+
+    var sum int
+	var total int
+    for _, v := range a {
+        sum += v
+		if v != 0 {
+			total += 1
+		}
+    }
+
+    t2 := time.Now()
+
+	n2, err := f.WriteString("Execution Time: " + t2.Sub(t1).String() + " Total:" + strconv.Itoa(total) + " Sum: " + strconv.Itoa(sum) + "\n" + "Top Ten Primes:\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n2)
+
+	var count int
+	for j := len(a) - 1; j >= 0; j-- {
+		if a[j] != 0 {
+			n2, err := f.WriteString(strconv.Itoa(a[j]) + "\n")
+			check(err)
+			fmt.Printf("wrote %d bytes\n", n2)
+			count += 1
+
+			if count >= 10 {
+				break
+			}
+		}
+	}
+
+	f.Sync()
 }
---
+++ assignments/001/primesReadMe.txt
@@ -0,0 +1,14 @@
+- The assignment was done in "go"
+- To run "go" must be installed and must be in your path
+
+Steps:
+1) cd into the directory with the file "primes.go"
+2) type "go run primes.go"
+3) an output.txt will be created with the specified outputs
+
+Note:
+- I had a tough time getting it to be efficient past 1,000,000
+so 1,000,000 is the default setting
+- Attempting 100,000,000 takes about 21 minutes
+- You will also notice that the prompt will have outputs saying "wrote __ bytes"
+this is just specifying the number of bytes written to the file at one time.
