7db25a487e76f0503cf46daefc3fa861a98aaf8a
“lrosa007” <lrosa008@gmail.com>
2016-03-10 22:37:58 -0500

    git ignore and goswitch changes

--- assignments/003/goswitch.sh
+++ assignments/003/goswitch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # GoSwitch: Portable Go Projects by Iyobo Eki
-# 
+#
 # A little script to help you manage your various Golang Projects and their individual GoPaths.
 # Simply put this script in the root directory of all your various GoPaths and run it with "source goswitch.sh" to set that directory as your current active go project directory (or gopath).
 # The script will also add the bin folder of your project into your system path variable so you can run all your bins as you would any command line command e.g. 'beego run'
@@ -36,7 +36,7 @@ path_append $GOPATH/bin
 export GOROOT=/usr/local/opt/go/libexec
 
 #echo "source $GOPATH/goswitch.sh" >> $gsbashrc
-echo "source $GOPATH/goswitch.sh" >> $gsbashprofile
+#echo "source $GOPATH/goswitch.sh" >> $gsbashprofile
 
 
 #echo "    Your Gopath is now $(tput setaf 6)$GOPATH.$(tput sgr 0) "
