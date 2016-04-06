1bdf3d5504c7f17e00bf7b2604afed4f4dc34150
“lrosa007” <lrosa008@gmail.com>
2016-04-06 15:28:35 -0400

    starting presentation

---
+++ assignments/004/main.cc
@@ -0,0 +1,21 @@
+#include <threads.h>
+
+user_main(int argc, char *argv[]) {
+
+}
+
+static void work(void *obj) {
+
+}
+
+static void add(void *obj) {
+
+}
+
+static void remove(void *obj) {
+
+}
+
+static void contain(void *obj) {
+  
+}
